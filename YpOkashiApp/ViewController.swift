9a46fdc8b388307b24932582d1aca037ec91778f
yoppen <yopppen.4403@gmail.com>
2021-06-25 10:20:15 +0900

    Initial Commit

---
+++ YpOkashiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YpOkashiApp
+//
+//  Created by 山岸善将 on 2021/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
